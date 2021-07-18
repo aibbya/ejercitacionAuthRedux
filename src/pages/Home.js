25936a249f32bb9a043f5dfacdbd2e8c3dc428bc
aibbya <aibbyale@gmail.com>
2021-07-18 02:42:25 -0300

    separacion del formulario del componente Login

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react'
+import userIsLogged from '../features/auth/userIsLogged';
+
+const Home = () => {
+    const [modo, setModo] = useState(false)
+
+    useEffect( ()=> {
+        setModo(userIsLogged())
+      }, [modo]);
+
+    return (
+        modo ? (
+        <div>
+            HOLA!! SOLO ESTAS ACA PORQUE ESTAS LOGUEADO
+        </div>)
+        :
+        (
+            <div> HOLA!! DEBES INICIAR SESION </div>
+        )
+    )
+}
+
+export default Home
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,152 +1,48 @@
-import { useState } from "react";
-import {
-    Flex,
-    Heading,
-    Input,
-    Button,
-    InputGroup,
-    Stack,
-    InputLeftElement,
-    chakra,
-    Alert,
-    AlertIcon,
-    Box,
-    Link,
-    Avatar,
-    FormControl,
-    FormHelperText,
-    InputRightElement
-} from "@chakra-ui/react";
-import { FaUserAlt, FaLock } from "react-icons/fa";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { useDispatch } from "react-redux";
-import { signInUser } from "../features/auth/authSlice";
+import React, { useState, Fragment, useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import FormLogin from "../features/auth/components/form/FormLogin";
+import { Redirect } from 'react-router-dom';
 
-const CFaUserAlt = chakra(FaUserAlt);
-const CFaLock = chakra(FaLock);
-const schema = Yup.object({
-    email: Yup.string()
-        .email("El email no es válido")
-        .required("El Email es Obligatorio"),
-    password: Yup.string()
-        .required("Ingrese su contraseña")
-})
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
+   
+    const auth = useSelector(state => state.auth.state)
+    const toast = useToast()
+    const [active, setActive] = useState(false)
 
-    const handleShowClick = () => setShowPassword(!showPassword);
-    const dispatch = useDispatch();
-
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        validationSchema: schema,
-        onSubmit: valores => {
-            const values = {
-                email: valores.email,
-                password: valores.password,
-            };
-            console.log(values);
-            debugger
-            dispatch(signInUser(values));
-        },
-    });
-
-
-
+    useEffect(() => {
+        if (auth ==="success"){
+            toast({
+                title: "Bienvenido!",
+                status: "success",
+                duration: 3000,
+              })
+              setTimeout(() => {
+                  setActive(true)                  
+              }, 3500);
+              
+        } else if (auth === "error"){
+            toast({
+                title: "Ups.. Usuario y/o contraseña incorrecta",
+                status: "error",
+                duration: 3000,
+              })
+        }
+    }, [auth]);  
     return (
-        <Flex
-            flexDirection="column"
-            width="100wh"
-            height="100vh"
-            backgroundColor="gray.200"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Stack
-                flexDir="column"
-                mb="2"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Avatar bg="teal.500" />
-                <Heading color="teal.400">Welcome</Heading>
-                <Box minW={{ base: "90%", md: "468px" }}>
-                    <form onSubmit={formik.handleSubmit}>
-                        <Stack
-                            spacing={4}
-                            p="1rem"
-                            backgroundColor="whiteAlpha.900"
-                            boxShadow="md"
-                        >
-                            <FormControl>
-                                <InputGroup>
+        <Fragment>
+        {
+            active ?
+            ( <Redirect to="/home"></Redirect>)
+            :
+            (<FormLogin></FormLogin> )
+        }
 
-                                    <InputLeftElement
-                                        pointerEvents="none"
-                                        children={<CFaUserAlt color="gray.300" />}
-                                    />
-                                    <Input
-                                        type="email" placeholder="email address"
-                                        id="email"
-                                        value={formik.values.email}
-                                        onChange={formik.handleChange}
-                                        onBlur={formik.handleBlur} />
-                                </InputGroup>
-                                {formik.touched.email && formik.errors.email ? (
-                                    <Alert justifyContent="center" status="error">
-                                        <AlertIcon />
-                                        {formik.errors.email}
-                                    </Alert>
-                                ) : null}
-                            </FormControl>
-                            <FormControl>
-                                <InputGroup>
-                                    <InputLeftElement
-                                        pointerEvents="none"
-                                        color="gray.300"
-                                        children={<CFaLock color="gray.300" />}
-                                    />
-                                    <Input
-                                        type={showPassword ? "text" : "password"}
-                                        placeholder="Password"
-                                        id="password"
-                                        name="password"
-                                        value={formik.values.password}
-                                        onChange={formik.handleChange}
-                                        onBlur={formik.handleBlur} />
-                                    />
-                                    <InputRightElement width="4.5rem">
-                                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                                            {showPassword ? "Hide" : "Show"}
-                                        </Button>
-                                    </InputRightElement>
-                                </InputGroup>                                
-                                {formik.touched.password && formik.errors.password ? (
-                                    <Alert justifyContent="center" status="error">
-                                        <AlertIcon />
-                                        {formik.errors.password}
-                                    </Alert>
-                                ) : null}
-                            </FormControl>
-                            <Button
-                                borderRadius={0}
-                                type="submit"
-                                variant="solid"
-                                colorScheme="teal"
-                                width="full"
-                            >
-                                Login
-                            </Button>
-                        </Stack>
-                    </form>
-                </Box>
-            </Stack>            
-        </Flex>
+
+        // 
+        
+        </Fragment>
     )
 }
 
