1395fd8c38527a717afff4d19503ce8b088ee2e2
aibbya <aibbyale@gmail.com>
2021-07-18 03:02:43 -0300

    reubicacion de archivos

---
+++ src/features/auth/components/form/FormLogin.js
@@ -0,0 +1,144 @@
+import React, { useState} from "react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Alert,
+  AlertIcon,
+  Box,
+  Avatar,
+  FormControl,
+  InputRightElement
+} from "@chakra-ui/react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { useFormik } from "formik";
+import { useDispatch} from "react-redux";
+import { singIn } from '../../services/authService';
+import {Schema} from '../validationSchema/schemaFormLogin'
+
+const FormLogin = () => {
+  const CFaUserAlt = chakra(FaUserAlt);
+  const CFaLock = chakra(FaLock);
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Schema,
+    onSubmit: valores => {
+      const values = {
+        email: valores.email,
+        password: valores.password,
+      };
+      console.log(values);
+      dispatch(singIn(values))
+      setTimeout(() => {
+        formik.resetForm();
+      }, 3000);
+      
+    },
+  });
+
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor="gray.200"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="teal.500" />
+        <Heading color="teal.400">Welcome</Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form onSubmit={formik.handleSubmit}>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            >
+              <FormControl>
+                <InputGroup>
+
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="gray.300" />}
+                  />
+                  <Input
+                    type="email" placeholder="email address"
+                    id="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur} />
+                </InputGroup>
+                {formik.touched.email && formik.errors.email ? (
+                  <Alert justifyContent="center" status="error">
+                    <AlertIcon />
+                    {formik.errors.email}
+                  </Alert>
+                ) : null}
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    id="password"
+                    name="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur} />
+                                    />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                      {showPassword ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                {formik.touched.password && formik.errors.password ? (
+                  <Alert justifyContent="center" status="error">
+                    <AlertIcon />
+                    {formik.errors.password}
+                  </Alert>
+                ) : null}
+              </FormControl>
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="teal"
+                width="full"
+              >
+                Login
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
+  )
+
+}
+export default FormLogin
+
---
+++ src/features/auth/services/authSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// AUTH STATE
+const authState = {
+    token: "",
+    error: "",
+    user: {},
+    loading: false,
+    state: null,
+};
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: authState,
+    reducers: {
+        logSuccess: (state, action) => {
+            localStorage.setItem("token", action.payload.token);
+            state.token = action.payload.token;
+            state.user = action.payload.user;
+            state.loading = false;
+            state.error = ""
+            state.state = "success";
+
+        },
+        logError: (state, action) => {
+            state.error = action.payload.message;
+			state.state = "error";
+			state.loading = false;
+            state.token = '';
+			localStorage.removeItem("token")
+        } 
+        
+    },
+})
+
+export const { logSuccess, logError } = authSlice.actions
+
+export const selectAuth = state => state.auth;
+
+export default authSlice.reducer
